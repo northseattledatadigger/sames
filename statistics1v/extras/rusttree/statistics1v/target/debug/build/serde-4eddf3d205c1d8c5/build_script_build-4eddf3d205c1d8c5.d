f8042e16c395cf6ece81892ebdfb66500188f210
Richard Burgess (also TAX) <northseattledatadigger@gmail.com>
2023-12-19 13:55:45 -0800

    Set aside enum vec version and go back to trait objects.

--- statistics1v/extras/rusttree/statistics1v/target/debug/build/serde-4eddf3d205c1d8c5/build_script_build-4eddf3d205c1d8c5.d
+++
@@ -1,5 +0,0 @@
-/home/nsdd/shop/sames/statistics1v/extras/rusttree/statistics1v/target/debug/build/serde-4eddf3d205c1d8c5/build_script_build-4eddf3d205c1d8c5: /home/nsdd/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.193/build.rs
-
-/home/nsdd/shop/sames/statistics1v/extras/rusttree/statistics1v/target/debug/build/serde-4eddf3d205c1d8c5/build_script_build-4eddf3d205c1d8c5.d: /home/nsdd/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.193/build.rs
-
-/home/nsdd/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.193/build.rs:
--- statistics1v/extras/rusttree/statistics1v/target/debug/deps/statistics1v-aa3b2b202d4385ff.d
+++
@@ -1,5 +0,0 @@
-/home/nsdd/shop/sames/statistics1v/extras/rusttree/statistics1v/target/debug/deps/statistics1v-aa3b2b202d4385ff: src/lib.rs
-
-/home/nsdd/shop/sames/statistics1v/extras/rusttree/statistics1v/target/debug/deps/statistics1v-aa3b2b202d4385ff.d: src/lib.rs
-
-src/lib.rs:
